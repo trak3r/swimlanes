2f29a5f6eae9fe3ddc15d642aed76a78f73a303a
Thomas "Teflon Ted" Davis <github@rudiment.net>
2013-09-15 13:32:46 -0400

    fresh rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swimlanes::Application.config.secret_key_base = 'b5cbd703e7752414f5634473109ed4b3da265333ea5d8f448d6b4b9a3edb68188bed827cad049f471e541cf3e425c201249b0b5d366143a613b2f822ca769053'
